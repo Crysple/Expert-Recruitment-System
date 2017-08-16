b8bf6917615f2285f3d7d81b75b327be318aa165
Eesr <871886504@qq.com>
2017-08-16 14:22:49 +0800

    master

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
+//some variables and initialization
 
+
+
+
+// functionality
 function addcertificate(){
 	$("#certificate").append('<div class="row db"><div class="col-xs-4"><input class="form-control ipstyle"></div><div class="col-xs-4"><input class="form-control ipstyle"></div><div class="col-xs-4"><button type="submit" class="btn btn-xs btn-default nbtn">删除</button></div></div>');
 }
@@ -14,26 +19,58 @@ function addavoidunit(){
 function del(){
 	$(this).parent().parent().remove();
 }
+function left_expertinfo(){
+	$("#menu1").show();
+	$("#li-menu1").show();
+	$("#a-menu1").click();
+}
+function left_changepassword(){
+	$("#menu2").show();
+	$("#li-menu2").show();
+	$("#a-menu2").click();
+}
 $(document).ready(function(){
-  $("button#addcertificate").click(addcertificate);
-  $("button#addassess").click(addassess);
-  $("button#addworkexperience").click(addworkexperience);
-  $("button#addavoidunit").click(addavoidunit)
+
+
+
+	$("button#addcertificate").click(addcertificate);
+	$("button#addassess").click(addassess);
+	$("button#addworkexperience").click(addworkexperience);
+	$("button#addavoidunit").click(addavoidunit);
+//left side
+	$("#left-expertinfo").click(left_expertinfo);
+	$("#left-changepassword").click(left_changepassword);
+	$('#left-home').click(function(){$('#top-home').click();})
+
+
+  //Tab
+	$('#tab-list').on('click','.close',function(){
+		var tabID = $(this).parents('a').attr('href');
+		$(this).parents('li').hide();
+		$(tabID).hide();
+
+		//display first tab
+		var tabFirst = $('#tab-list a:first');
+		tabFirst.tab('show');
+	});
 });
+
+
+
 
 //certificate
 $(document).on('click', 'button#addcertificate',function() {
 	$(".nbtn").click(del) ;
-	}) ;
-	//assess
-	$(document).on('click', 'button#addassess',function() {
+}) ;
+//assess
+$(document).on('click', 'button#addassess',function() {
 	$(".delassess").click(del) ;
-	}) ;
-	//work experience
-	$(document).on('click', 'button#addworkexperience',function() {
+}) ;
+//work experience
+$(document).on('click', 'button#addworkexperience',function() {
 	$(".delworkexperience").click(del) ;
-	}) ;
-	//avoid unit
-	$(document).on('click', 'button#addavoidunit',function() {
-	$(".delavoidunit").click(del) ;
-	}) ;
+}) ;
+//avoid unit
+$(document).on('click', 'button#addavoidunit',function() {
+$(".delavoidunit").click(del) ;
+}) ;
